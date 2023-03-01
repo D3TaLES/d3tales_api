7f9b86396ba13f5d74fee82d3cd446781d0613b0
Rebekah Duke <rduke199@gmail.com>
2023-03-01 08:52:14 -0500

    add mol conformer search

--- d3tales_api/D3database/info_from_smiles.py
+++ d3tales_api/D3database/info_from_smiles.py
@@ -1,16 +1,66 @@
 import base64
 import selfies as sf
 from PIL import Image
+from tqdm import tqdm
 from io import BytesIO
 import pubchempy as pcp
 from d3tales_api.D3database.schema2class import Schema2Class
 
+from rdkit import Chem
 from rdkit.Chem.rdchem import Mol
 from rdkit.Chem.rdmolops import GetFormalCharge, AddHs
 from rdkit.Chem.Descriptors import NumRadicalElectrons
 from rdkit.Chem.inchi import MolToInchi, MolToInchiKey
 from rdkit.Chem import Draw, MolFromSmiles, MolToSmiles, AllChem
 from rdkit.Chem.rdMolDescriptors import CalcMolFormula, CalcExactMolWt
+
+
+def find_lowest_e_conf(smiles, num_conf=50):
+    """
+    Find the lowest energy conformer for a molecule with RDKit
+    :param smiles: str, SMILES string
+    :param num_conf: float, number of conformers to serach
+    :return: str, xyz coordinates
+    """
+    rdkmol = Chem.AddHs(Chem.MolFromSmiles(smiles))
+    results = {}
+    AllChem.EmbedMultipleConfs(rdkmol, numConfs=num_conf, params=AllChem.ETKDG())
+    results_MMFF = AllChem.MMFFOptimizeMoleculeConfs(rdkmol, maxIters=5000)
+    for i, result in tqdm(enumerate(results_MMFF)):
+        results[i] = result[1]
+    best_idx = min(results, key=results.get)
+    structure = Chem.rdmolfiles.MolToXYZBlock(rdkmol, confId=best_idx)
+    return structure
+
+
+def image_to_base64(img):
+    """
+    Produce base 64 string representation of image
+
+    :param img: python Image object
+    :return: str representing the image
+    """
+    output_buffer = BytesIO()
+    img.save(output_buffer, format='PNG')
+    byte_data = output_buffer.getvalue()
+    base64_str = base64.b64encode(byte_data)
+    return base64_str.decode('ascii')
+
+
+def base64_to_image(base64_str, image_path=None):
+    """
+    Produce python Image object from base 64 string representation of image
+
+    :param base64_str: base64 string representing image
+    :param image_path: path for image to be saved (optional)
+    :return: python Image object
+    """
+    byte_data = base64.b64decode(base64_str)
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    if image_path:
+        img.save(image_path)
+    return img
 
 
 class GenerateMolInfo:
@@ -62,8 +112,8 @@ class GenerateMolInfo:
         mol_info.groundState_charge = GetFormalCharge(rdkmol)
         mol_info.groundState_spin = NumRadicalElectrons(rdkmol) + 1  # calculate spin multiplicity with Hand's rule
         if self.extra_info:
-            mol_info.d2_image = self.image_to_base64(Draw.MolToImage(rdkmol))
-            mol_info.init_structure = AllChem.rdmolfiles.MolToXYZBlock(rdkmol_hs)
+            mol_info.d2_image = image_to_base64(Draw.MolToImage(rdkmol))
+            mol_info.init_structure = find_lowest_e_conf(clean_smile)
         try:
             mol_info.synonyms = self.names + pcpmol.synonyms
         except TypeError:
@@ -71,32 +121,3 @@ class GenerateMolInfo:
 
         return mol_info.as_dict()
 
-    @staticmethod
-    def image_to_base64(img):
-        """
-        Produce base 64 string representation of image
-
-        :param img: python Image object
-        :return: str representing the image
-        """
-        output_buffer = BytesIO()
-        img.save(output_buffer, format='PNG')
-        byte_data = output_buffer.getvalue()
-        base64_str = base64.b64encode(byte_data)
-        return base64_str.decode('ascii')
-
-    @staticmethod
-    def base64_to_image(base64_str, image_path=None):
-        """
-        Produce python Image object from base 64 string representation of image
-
-        :param base64_str: base64 string representing image
-        :param image_path: path for image to be saved (optional)
-        :return: python Image object
-        """
-        byte_data = base64.b64decode(base64_str)
-        image_data = BytesIO(byte_data)
-        img = Image.open(image_data)
-        if image_path:
-            img.save(image_path)
-        return img
