c9dae7425e721c4bc5e081785b7bf044b5a1e9fa
Rebekah Duke <rduke199@gmail.com>
2023-06-16 10:44:57 -0400

    add back2front for CV

--- d3tales_api/Processors/back2front.py
+++ d3tales_api/Processors/back2front.py
@@ -584,6 +584,175 @@ class DOI2Front:
         return cls(doi=doi, backend_data=processing_data, **kwargs)
 
 
+class CV2Front:
+    """
+    Update frontend db with backend db data for a particular set of data.
+    Copyright 2021, University of Kentucky
+    """
+
+    def __init__(self, id_list=None, backend_data=None, mol_id=None, e_half_scan_rate=0.1, run_anodic=False,
+                 run_processing=True, insert=True, verbose=1):
+        """
+        :param id_list: list, list of backend ids
+        :param backend_data: list, list of JSON containing processed backend data
+        :param mol_id: str, default molecule ID for data if none found  
+        :param e_half_scan_rate: float, scan rate at which to extract e half
+        :param run_anodic: bool, run processing for anodic sweep if True
+        :param run_processing: bool, run processing (takes a few minutes) if True
+        :param insert: bool, insert generated data to the frontend D3TaLES database if True
+        :param verbose: int, level of verbosity
+        """
+
+        # connect to databases
+        self.front_coll = DBconnector(db_info.get("frontend")).get_collection("base")
+        self.back_coll = DBconnector(db_info.get("backend")).get_collection("experimentation")
+
+        # Basic variables
+        self.verbose = verbose
+        if not id_list and not backend_data:
+            raise ValueError("The CV2Front class requires either the 'id_list' kwarg or 'backend_data' kwarg. Neither were provided. ")
+        self.multi_data = backend_data or [self.back_coll.find_one({"_id": i}) for i in id_list]
+        if not self.multi_data or not self.multi_data[0]:
+            raise ValueError("No backend data found for IDs {} and no backend data provided.".format(id_list))
+        self.conditions = self.get_conditions()
+        self.p_ids = [d.get("_id") for d in self.multi_data]
+        self.e_half_scan_rate = e_half_scan_rate
+        self.run_anodic = run_anodic
+
+        # Check Mol ID
+        mol_ids = list(set(filter(lambda i: i is not None, [d.get("mol_id") for d in self.multi_data])))
+        if len(mol_ids) > 1: 
+            raise ValueError("Not all submitted data is associated with the same moleucle! Submitted data are associated"
+                             "with molecules {}.".format(", ".join(mol_ids)))
+        self.mol_id = mol_ids[0] if mol_ids else mol_id
+        self.meta_dict = {}
+
+        if run_processing:
+            self.process()
+
+        [setattr(self, k, v) for k, v in self.meta_dict.items()]
+
+        if insert:
+            FrontDB(schema_layer="experiment_data.mol_characterization", instance=self.meta_dict, _id=self.mol_id)
+            FrontDB(instance={"experiment_data.experiment_ids": self.p_ids}, _id=data.get("mol_id"))
+
+    def process(self):
+        if self.verbose:
+            print("STARTING META PROPERTY PROCESSING...This could take a few minutes.")
+            print("E 1/2s: ", self.e_halfs)
+
+        for i, e_half in enumerate(self.e_halfs):
+            c_diffusion_coef, c_transfer_rate = self.cv_meta_calcs(electron_num=i + 1, curve_type="cathodic")
+            self.meta_dict.update(self.return_descriptor_dict(
+                c_diffusion_coef[1], unit="cm^2/s", name="diffusion_coefficient", order=i, notes="cathodic"))
+            self.meta_dict.update(self.return_descriptor_dict(
+                c_transfer_rate, unit="cm/s", name="charge_transfer_rate", order=i, notes="cathodic"))
+            if self.run_anodic:
+                a_diffusion_coef, a_transfer_rate = self.cv_meta_calcs(electron_num=i + 1, curve_type="anodic")
+                self.meta_dict.update(self.return_descriptor_dict(
+                    a_diffusion_coef[1], unit="cm^2/s", name="diffusion_coefficient", order=i, notes="anodic"))
+                self.meta_dict.update(self.return_descriptor_dict(
+                    a_transfer_rate, unit="cm/s", name="charge_transfer_rate", order=i, notes="anodic"))
+
+    def get_conditions(self):
+        all_cond = [d.get("data", {}).get("conditions") for d in self.multi_data]
+        if not all_cond:
+            raise ValueError("No experiments have associated conditions.")
+        return {k: v for k, v in all_cond[0].items() if len(set([self.generate_hash(c[k]) for c in all_cond]))==1}
+
+    @staticmethod
+    def generate_hash(hash_dict):
+        dhash = hashlib.md5()
+        encoded = json.dumps(hash_dict, sort_keys=True).encode()
+        dhash.update(encoded)
+        return dhash.hexdigest()
+
+    def return_descriptor_dict(self, value, unit="", name="", order=1, notes=None):
+        """
+        Generate descriptor dictionary in accordance with D3TaLES schema
+
+        :param value: data value
+        :param unit: str, unit
+        :param name: str, property name
+        :param order: int, property order
+        :param notes: str, property notes
+        :return: descriptor dictionary
+        """
+        cond = copy.deepcopy(self.conditions)
+        prop_dict = {
+                    "source_hash_ids": self.p_ids,
+                    "conditions": cond,
+                    "order": order,
+                    "value": value,
+                    "unit": unit
+                }
+        if notes:
+            prop_dict.update({"notes": notes})
+        return { name: [prop_dict] }
+
+
+    def cv_meta_calcs(self, electron_num=1, curve_type="anodic"):
+        processed_data = []
+        for i in self.multi_data:
+            try:
+                data_dict = i.get('data')
+                data_dict["n"] = electron_num
+                data_dict["current_cathodic"] = data_dict.get("forward", {})[electron_num - 1][1]
+                data_dict["current_anodic"] = data_dict.get("reverse", {})[electron_num - 1][1]
+                processed_data.append(data_dict)
+            except:
+                pass
+        connector = {
+            "n": "n",
+            "i_p": "current_{}".format(curve_type),
+            "X": "peak_splittings.{}".format(electron_num - 1),
+            "v": "conditions.scan_rate",
+            "T": "conditions.temperature",
+            "C": "conditions.redox_mol_concentration",
+            "A": "conditions.working_electrode_surface_area",
+
+            "D": "diffusion",
+            "scan_data": "middle_sweep",
+        }
+
+        try:
+            # Calculate diffusion constant
+            if self.verbose:
+                print("Calculating {} diffusion coefficient for oxidation {}...".format(curve_type, electron_num))
+            diffusion_cal = CVDiffusionCalculator(connector=connector)
+            diffusion_coef = diffusion_cal.calculate(processed_data, sci_notation=True)
+
+            # Calculate charge transfer rates
+            if self.verbose:
+                print("Calculating {} charge transfer rate for oxidation {}...".format(curve_type, electron_num))
+            [d.update({"diffusion": float(diffusion_coef[1])}) for d in processed_data]
+            transfer_cal = CVChargeTransferCalculator(connector=connector)
+            transfer_rate = transfer_cal.calculate(processed_data, sci_notation=True)
+            return diffusion_coef, transfer_rate
+        except:
+            return [None, None], None
+
+    @property
+    def e_halfs(self):
+        for d in self.multi_data:
+            d_sr = d.get("data", {}).get("conditions", {}).get("scan_rate", {}).get("value", None)
+            if self.e_half_scan_rate == d_sr:
+                return d.get("data", {}).get("e_half")
+
+    @classmethod
+    def from_json(cls, json_path, **kwargs):
+        """
+        Generate data class from data dict
+
+        :param json_path: str, path to JSON file
+        :return: data class
+        """
+        with open(json_path) as fn:
+            processing_data = json.load(fn)
+        multi_data = processing_data if isinstance(list, processing_data) else [processing_data]
+        return cls(backend_data=multi_data, **kwargs)
+
+
 if __name__ == "__main__":
     back_dbc = DBconnector(db_info.get("backend"))
     back_coll = back_dbc.get_collection("computation")
